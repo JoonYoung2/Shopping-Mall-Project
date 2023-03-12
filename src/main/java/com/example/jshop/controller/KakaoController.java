fab82d9d2d6b9265e9393c0133b39a633b44d899
JoonYoung2 <90246124+JoonYoung2@users.noreply.github.com>
2023-03-13 04:45:00 +0900

    method name update
    method name update

--- src/main/java/com/example/jshop/controller/KakaoController.java
+++ src/main/java/com/example/jshop/controller/KakaoController.java
@@ -34,7 +34,7 @@ public class KakaoController {
 	private MemberRepository repo;
 
 	@GetMapping("kakao_login")
-	public String redirectKakao(@RequestParam("code") String code, Model model) throws IOException {
+	public String Kakao_login(@RequestParam("code") String code, Model model) throws IOException {
 
 		// 접속토큰 get
 		String kakaoToken = service.getReturnAccessToken(code);
